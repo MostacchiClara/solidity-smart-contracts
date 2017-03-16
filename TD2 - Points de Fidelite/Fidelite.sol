80b659ce3067c0b4e8b569153b026a67c580be6a
unknown <clara.mostacchi@gmail.com>
2017-03-16 15:19:47 +0100

    Last commit

--- TD2 - Points de Fidelite/Fidelite.sol
+++ TD2 - Points de Fidelite/Fidelite.sol
@@ -1,5 +1,7 @@
-
 pragma solidity ^0.4.0;
+
+// Contract designed for fidelity point management
+
 contract Points {
 
     address chairperson;
@@ -8,118 +10,119 @@ contract Points {
 
     struct Client {
         address address_client;
-        uint points;
+        uint balance;
     }
 
     struct Cashier {
         address address_cashier;
     }
     
+    
+    // Initialise the chairperson of the contract
     function Points() {
         chairperson = msg.sender;
     }
-
-    // Allow voters to vote, take in argument the proposal
-    function isCashier()constant returns(bool isCashier)
-    {
-        for(uint i = 0; i< cashiers.length ; i++)
-        {
-            if(msg.sender == cashiers[i].address_cashier )
-            {
-                isCashier = true;
-                return isCashier;
-            }else{
-                isCashier = false;
-                return isCashier;
-            }
-        }
+    
+    
+    modifier isChairperson(){
+        if(msg.sender != chairperson)
+            throw; 
+            _;
+    }
+    
+    modifier isCashier(){
+        for(uint i = 0; i < cashiers.length ; i++ ){
+            if(cashiers[i].address_cashier == msg.sender)
+            _;
+        }throw;
     }
 
+    
+    function getBalance(uint _aIndex) isCashier() constant returns(uint) {
+        return clients[_aIndex].balance;
+    }
+
+
     // Allow to add a new Client to the list, can only be performed by cashier
-    function addClient(address client) {
+    function addClient(address _aClient)  isCashier(){
         bool present = false;
         
-        for(uint i = 0; i< clients.length ; i++)
-        {
-            if(client == clients[i].address_client)
-            {
+        for(uint i = 0; i< clients.length ; i++){
+            
+            if(_aClient == clients[i].address_client){
                 present = true;
             }
         }
         if(present)throw;
         else{
-                if(isCashier())
-                {
-                    clients.push(Client({address_client :client, points : 0}));
-                }
-                else{throw;}
-            }
+            clients.push(Client({address_client :_aClient, balance : 0}));
+        }
     }
     
     
-    // Add a new Cashier in the list of Cashier, only can performed by chairperson 
-    function addCashier(address cashier) {
+    // Add a new Cashier in the list of Cashier, only can be performed by chairperson 
+    function addCashier(address _aCashier) isChairperson() {
+        
         bool present = false;
-        if (msg.sender != chairperson)
-        {
-            throw;
-        }
-        for(uint i = 0; i< cashiers.length ; i++)
-        {
-            if(cashier == cashiers[i].address_cashier)
-            {
+        for(uint i = 0; i< cashiers.length ; i++){
+            
+            if(_aCashier == cashiers[i].address_cashier){
                 present = true ;
             }
         }
-        if(!present)
-        {
-            cashiers.push(Cashier({address_cashier : cashier}));
+        if(!present){
+            cashiers.push(Cashier({address_cashier : _aCashier}));
         }
-
-    }
-    // Allow to add a new proposal to proposals tab this operation can be only
-    // perform by the chairman
-    // Takes in parameter the name of the proposal
-	
-    function addPoint(address client, uint points) {
-        if (isCashier() == true)
-        {
-            for(uint i  = 0 ; i < clients.length; i++)
-            {
-                if(clients[i].address_client == client)
-                {
-                    clients[i].points += points;
-                }
-            }
-        }else throw;
     }
     
     
-    function usePoint(address client, uint points) {
-        if (isCashier() == true)
-        {
-           for(uint i  = 0 ; i < clients.length; i++)
-            {
-                if(clients[i].address_client == client)
-                {
-                    if(clients[i].points > points)
-                    {
-                        clients[i].points = clients[i].points - points;
-                    }else{
-                        
-                        throw;
-                    }
+	// Allow cashier to add point on user account 
+    function addPoint(address _aClient, uint _aPoints) isCashier() {
+        
+        for(uint i  = 0 ; i < clients.length; i++){
+                
+            if(clients[i].address_client == _aClient){
                     
-                }
+                clients[i].balance += _aPoints;
             }
-        }else throw;
-    }
+            else{ throw; } 
+        }
+    } 
     
+    // Allow Cashier to use point for future payments 
+    // Only Cashier can perform the operation 
+    function usePoint(address _aClient, uint _aPoints) isCashier() {
+            
+        for(uint i  = 0 ; i < clients.length; i++){
+               
+            if(clients[i].address_client == _aClient){
+                    
+                if( getBalance(i) > _aPoints){
+                        
+                    clients[i].balance = clients[i].balance - _aPoints;
+                }else{
+                    throw;
+                }
+                    
+            }
+        }
+    }
 
     
-    function kill() {
-        if (msg.sender == chairperson) {
-            suicide(chairperson);
-        }else{ throw;}
+    function balanceOf(address _aClient) isCashier() constant returns(uint){
+        
+        for(uint i  = 0 ; i < clients.length; i++){
+            
+            if(clients[i].address_client == _aClient){
+                
+               return getBalance(i);
+            }
+        }
+        throw;
     }
+    
+   function kill() isChairperson() {
+            suicide(msg.sender);
+    }
+    
 }
